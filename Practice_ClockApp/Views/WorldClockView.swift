a4ba22679f68731c308d28f893d06e441a559b8c
Ella Sehn <esehn@lcs.on.ca>
2026-03-03 13:49:56 -0500

    Finnished WorldClock

--- Practice_ClockApp/Views/WorldClockView.swift
+++ Practice_ClockApp/Views/WorldClockView.swift
@@ -11,50 +11,73 @@ struct WorldClockView: View {
     var body: some View {
         NavigationStack {
             VStack{
-                HStack {
-                    VStack {
-                        Text("Today, +0HRS")
-                        Text("Ottawa")
-                            .font(.system(.largeTitle, design: .default, weight: .thin))
+                
+                ExtractedView(timeZoneOffset: "+0HRS",
+                              city: "Ottawa",
+                              time: "6:35",
+                              amOrPm: "AM")
+                
+                ExtractedView(timeZoneOffset: "-3HRS",
+                              city: "Vancouver",
+                              time: "3:35",
+                              amOrPm: "AM")
+                
+                ExtractedView(timeZoneOffset: "+6HRS",
+                              city: "Hamburg",
+                              time: "12:35",
+                              amOrPm: "PM")
+                
+                ExtractedView(timeZoneOffset: "+16HRS",
+                              city: "Sydney",
+                              time: "18:35",
+                              amOrPm: "PM")
+                
+            }
+            
+            .navigationTitle("World Clock")
+            .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    
+                    Button("Edit") {
+    
                     }
-                    
-                    Spacer()
-                    
-                    Text("6:35")
-                        .font(.system(size: 64.0, weight: .thin, design: .default))
-                    Text("AM")
-                        .font(.system(.largeTitle, design: .default, weight: .thin))
                     
                 }
-            Text("World Clock")
-                .navigationTitle("World Clock")
-                .toolbar {
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                        
-                        Button("Edit") {
-                            // Does nothing right now
-                        }
-                        
-                    }
-                    
-                    ToolbarItem(placement: .topBarLeading) {
-                            
-                            Button("Edit") {
-                                // Does nothing right now
-                            }
-                            
-                        }
-                        
-                    }
-
-                    
-                }
-
+                
+                
+                #Preview {
+                    LandingView()}
+                
+            }
         }
     }
-
-
-#Preview {
-    LandingView()
+    
+    struct ExtractedView: View {
+        let timeZoneOffset: String
+        let city: String
+        let time: String
+        let amOrPm: String
+        
+        var body: some View {
+            HStack {
+                VStack {
+                    Text(timeZoneOffset)
+                    Text(city)
+                        .font(.system(.largeTitle, design: .default, weight: .thin))
+                }
+                
+                Spacer()
+                
+                Text(time)
+                    .font(.system(size: 64.0, weight: .thin, design: .default))
+                Text(amOrPm)
+                    .font(.system(.largeTitle, design: .default, weight: .thin))
+            }
+        }
+    }
+    
+    
 }
+
+
