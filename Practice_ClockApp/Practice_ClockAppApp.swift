d8535fdc2cdd35c078b72d3a912c17d839014be2
Ella Sehn <esehn@lcs.on.ca>
2026-03-02 23:39:58 -0500

    Initial Commit

---
+++ Practice_ClockApp/Practice_ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_ClockAppApp.swift
+//  Practice_ClockApp
+//
+//  Created by Ella Sehn on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct Practice_ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
